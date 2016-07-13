e88d5a704ca600dafc4a28144348bedcc5c19c33
Jimmie Jackson <jimmiejackson414@gmail.com>
2016-07-12 21:27:24 -0500

    added database inventory and created js

--- bamazon.sql
+++ bamazon.sql
@@ -10,3 +10,14 @@ CREATE TABLE products (
     stockQuantity INT NULL,
     PRIMARY KEY (`itemID`)
 );
+
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Sugar-Free Gummy Bears', 'Food', 5, 20);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Canon 24-105mm Lens', 'Photography', 800, 5);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Black Capo', 'Music', 4, 50);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Pocky Chocolate Sticks', 'Food', 2, 200);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('L-Bracket', 'Photography', 75, 20);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Assorted Guitar Picks', 'Music', 2, 50);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Vegemite', 'Food', 10, 0);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Sling Strap', 'Photography', 15, 30);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Guitar Polish', 'Music', 5, 100);
+INSERT INTO products (productName, departmentName, price, stockQuantity) VALUES ('Freeze Dried Ice Cream', 'Food', 10, 80);
